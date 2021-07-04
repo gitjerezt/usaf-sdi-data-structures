75ce51e39c73746445f3bf558da4d776959c9724
Ty Jereza <nexusspark.jerezt@gmail.com>
2021-07-04 15:14:10 -0700

    Sprint 2: Solved tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,35 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+    // fix me
+  Object.assign(newTree, treeMethods);
+  newTree.children = null;
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+ if (this.children === null) {
+   this.children = [];
+ }
+ this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  let findValue = function(tree, target) {
+    let result = false;
+    if (tree.value === target) {
+      result = true;
+    } else if (tree.children !== null){
+        for (let i = 0; i < tree.children.length; i++) {
+           result = result || findValue(tree.children[i], target);
+        }
+    }
+    return result;
+  }
+  return findValue(this, target);
 };
-
 
 
 /*
