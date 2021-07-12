9ddbb775d82e7075bf074955675e8da6bf1774bf
Ty Jereza <nexusspark.jerezt@gmail.com>
2021-07-11 20:56:47 -0700

    Solved Sprint 2: Set and Hast Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,23 +1,44 @@
-
+///////////TYPE BELOW HERE!
 
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, new LinkedList());
+  }
+  this._storage.get(index).addToTail(v);
 };
-
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var hashCell = this._storage.get(index);
+  var result = undefined;
+  if(hashCell !== undefined) {
+    var head = hashCell.head;
+    var tail = hashCell.tail;
+    if(head === null) {
+      result = undefined;
+    } else if (head === tail){
+      result = head.value;
+    } else if(head !== tail && !hashCell.contains(k)) {
+      result = tail.value;
+    } else {
+      if(hashCell.contains(k)) {
+        result = k;
+      }
+    }
+  }
+  return result;
 };
-
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
+  delete this._storage.get(index);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,27 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(this._storage === null) {
+    this._storage = {};
+  }
+  if (!this.contains(item)) {
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (let key in this._storage) {
+    if(this._storage[key].normalize() === item.normalize()) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if(this.contains(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
